cb95800201d18341238d9da75cb20cf9333ccf52
matthias zimmermann <zimmermann@xbrain.ch>
2022-06-20 19:42:38 +0200

    adapt TestOracle to changed Oracle implementation in gif-interface

--- contracts/test/TestOracle.sol
+++ contracts/test/TestOracle.sol
@@ -12,21 +12,34 @@ contract TestOracle is Oracle {
         Oracle(oracleName, registry)
     { }
 
-    function _calculateResponseData(bytes calldata requestData)
-        internal 
-        override 
-        returns (bytes memory responseData)
-    {
+    function request(uint256 requestId, bytes calldata input) external override onlyQuery {
         // decode oracle input data
-        (uint256 input_value) = abi.decode(requestData, (uint256));
+        (uint256 input_value) = abi.decode(input, (uint256));
 
-        // this is just for dummy testing
-        // real oracle implementations would make the oracle call here
-        // either to some chainlink data feed or some off-chain
-        // component providing the outcome of the business logic
-        bool isLossEvent = (input_value % 2 == 1);
-
-        // encode oracle output data
-        responseData = abi.encode(bool(isLossEvent));
+        // obtain data from oracle given the request data (input_value)
+        // for off chain oracles this happens outside the request
+        // call in a separate asynchronous transaction
+        bool isLossEvent = _oracleCalculation(input_value);
+        respond(requestId, isLossEvent);
     }
+
+    // usually called by off-chain oracle (and not internally) 
+    // in which case the function modifier should be changed 
+    // to external
+    function respond(uint256 requestId, bool isLossEvent) 
+        internal
+    {
+        // encode data obtained from oracle
+        bytes memory output = abi.encode(bool(isLossEvent));
+
+        // trigger inherited response handling
+        _respond(requestId, output);
+    }
+
+    // dummy implementation
+    // "real" oracles will get the output from some off-chain
+    // component providing the outcome of the business logic
+    function _oracleCalculation(uint256 value) internal returns (bool isLossEvent) {
+        isLossEvent = (value % 2 == 1);
+    }    
 }
