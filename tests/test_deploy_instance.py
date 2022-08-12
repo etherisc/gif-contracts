9c1cd589d96082dfcbceb471b2462f4f6bcc7a35
Marc Doerflinger <doerfli@users.noreply.github.com>
2022-08-12 09:50:23 +0000

    Adapt unit test for new errors

--- tests/test_deploy_instance.py
+++ tests/test_deploy_instance.py
@@ -120,7 +120,7 @@ def test_PolicyDefaultFlow(instance: GifInstance, owner):
     assert policyDefaultFlow.address == policyDefaultFlowAddress
     assert policyDefaultFlow.address != 0x0
 
-    with brownie.reverts('ERROR:POC-050:METADATA_DOES_NOT_EXIST'):
+    with brownie.reverts('ERROR:POC-051:APPLICATION_DOES_NOT_EXIST'):
         policyDefaultFlow.getApplicationData(s2b32(''))
 
     with pytest.raises(AttributeError):
