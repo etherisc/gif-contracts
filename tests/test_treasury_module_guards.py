1afb4a58630f56d7ef14506f9412915b9337d2ea
Marc Doerflinger <marc@doerf.li>
2022-09-12 11:24:41 +0000

    add unit tests

--- tests/test_treasury_module_guards.py
+++ tests/test_treasury_module_guards.py
@@ -26,6 +26,7 @@ def test_guard_processPremium(
     riskpoolKeeper: Account,
     capitalOwner: Account,
     customer: Account,
+    theOutsider: Account,
 ):  
     applicationFilter = bytes(0)
 
@@ -64,11 +65,36 @@ def test_guard_processPremium(
 
     processId = apply_for_policy(instance, owner, product, customer, testCoin, premium, sumInsured)
 
+    # ensure processing premium is no possible from other than policy flow
     with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
         instance.getTreasury().processPremium(processId, {'from': customer})
     
     with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
         instance.getTreasury().processPremium(processId, premium, {'from': customer})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, {'from': productOwner})
+    
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, premium, {'from': productOwner})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, {'from': riskpoolKeeper})
+    
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, premium, {'from': riskpoolKeeper})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, {'from': instance.getInstanceService().getInstanceOperator()})
+    
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, premium, {'from': instance.getInstanceService().getInstanceOperator()})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, {'from': theOutsider})
+    
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPremium(processId, premium, {'from': theOutsider})
 
 
 def test_guard_processPayout(
@@ -80,6 +106,7 @@ def test_guard_processPayout(
     riskpoolKeeper: Account,
     capitalOwner: Account,
     customer: Account,
+    theOutsider: Account,
 ):  
     applicationFilter = bytes(0)
 
@@ -118,9 +145,136 @@ def test_guard_processPayout(
 
     processId = apply_for_policy(instance, owner, product, customer, testCoin, premium, sumInsured)
 
+    # ensure processing payout is no possible from other than policy flow
     with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
         instance.getTreasury().processPayout(processId, "123", {'from': customer})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPayout(processId, "123", {'from': productOwner})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPayout(processId, "123", {'from': riskpoolKeeper})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPayout(processId, "123", {'from': instance.getInstanceService().getInstanceOperator()})
+
+    with brownie.reverts("ERROR:CRC-003:NOT_PRODUCT_SERVICE"):
+        instance.getTreasury().processPayout(processId, "123", {'from': theOutsider})
     
+
+def test_guard_processCapital(
+    instance: GifInstance,
+    owner: Account,
+    testCoin,
+    productOwner: Account,
+    oracleProvider: Account,
+    riskpoolKeeper: Account,
+    capitalOwner: Account,
+    customer: Account,
+    theOutsider: Account,
+):  
+    applicationFilter = bytes(0)
+
+    # prepare product and riskpool
+    (gifProduct, gifRiskpool, gifOracle) = getProductAndRiskpool(
+        instance,
+        owner,
+        testCoin,
+        productOwner,
+        oracleProvider,
+        riskpoolKeeper,
+        capitalOwner,
+        True
+    )
+
+    # fund bundle
+    safetyFactor = 2
+    amount = 10000
+    testCoin.transfer(riskpoolKeeper, safetyFactor * amount, {'from': owner})
+    testCoin.approve(instance.getTreasury(), amount, {'from': riskpoolKeeper})
+    riskpool = gifRiskpool.getContract()
+
+    riskpool.createBundle(
+                applicationFilter, 
+                amount, 
+                {'from': riskpoolKeeper})
+
+    bundle = riskpool.getBundle(0)
+    print(bundle)
+
+    # ensure no withdrawal is possible from other than riskpoool service
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processCapital(bundle[0], "1000", {'from': customer})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processCapital(bundle[0], "1000", {'from': productOwner})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processCapital(bundle[0], "1000", {'from': riskpoolKeeper})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processCapital(bundle[0], "1000", {'from': instance.getInstanceService().getInstanceOperator()})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processCapital(bundle[0], "1000", {'from': theOutsider})
+
+
+def test_guard_processWithdrawal(
+    instance: GifInstance,
+    owner: Account,
+    testCoin,
+    productOwner: Account,
+    oracleProvider: Account,
+    riskpoolKeeper: Account,
+    capitalOwner: Account,
+    customer: Account,
+    theOutsider: Account,
+):  
+    applicationFilter = bytes(0)
+
+    # prepare product and riskpool
+    (gifProduct, gifRiskpool, gifOracle) = getProductAndRiskpool(
+        instance,
+        owner,
+        testCoin,
+        productOwner,
+        oracleProvider,
+        riskpoolKeeper,
+        capitalOwner,
+        True
+    )
+
+    # fund bundle
+    safetyFactor = 2
+    amount = 10000
+    testCoin.transfer(riskpoolKeeper, safetyFactor * amount, {'from': owner})
+    testCoin.approve(instance.getTreasury(), amount, {'from': riskpoolKeeper})
+    riskpool = gifRiskpool.getContract()
+
+    riskpool.createBundle(
+                applicationFilter, 
+                amount, 
+                {'from': riskpoolKeeper})
+
+    bundle = riskpool.getBundle(0)
+    print(bundle)
+
+    # ensure no withdrawal is possible from other than riskpoool service
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processWithdrawal(bundle[0], "1000", {'from': customer})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processWithdrawal(bundle[0], "1000", {'from': productOwner})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processWithdrawal(bundle[0], "1000", {'from': riskpoolKeeper})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processWithdrawal(bundle[0], "1000", {'from': instance.getInstanceService().getInstanceOperator()})
+
+    with brownie.reverts("ERROR:TRS-005:NOT_RISKPOOL_SERVICE"):
+        instance.getTreasury().processWithdrawal(bundle[0], "1000", {'from': theOutsider})
+
 
 
 def getProductAndRiskpool(
