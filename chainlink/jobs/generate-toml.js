c28b080482712f88ed104c92baad81c2c9e519f8
Christoph Mussenbrock <christoph@etherisc.com>
2022-09-16 12:02:38 +0000

    updated chainlink jobs

--- chainlink/jobs/generate-toml.js
+++ chainlink/jobs/generate-toml.js
@@ -5,19 +5,25 @@ const main = () => {
   const dotName = process.argv[3]
   const tomlName = process.argv[4]
 
-  console.log(`Generating TOML: Template ${templateName}, DOT ${dotName}, output to ${tomlName}`)
+  if (templateName && dotName && tomlName) {
+    console.log(`Generating TOML: Template ${templateName}, DOT ${dotName}, output to ${tomlName}`)
 
-  const template = fs.read(templateName)
-  const dot = fs.read(dotName)
-  const toml = template
-    .replace(
-      '"""\n"""',
-      dot
-        .replace('digraph {', '"""')
-        .replace('} //digraph', '"""')
-        .replaceAll('\\', '\\\\'),
-    )
-  fs.write(tomlName, toml)
+    const template = fs.read(templateName)
+    const dot = fs.read(dotName)
+    const toml = template
+      .replace(
+        '"""\n"""',
+        dot
+          .replace('digraph {', '"""')
+          .replace('} //digraph', '"""')
+          .replaceAll('\\', '\\\\'),
+      )
+    fs.write(tomlName, toml)
+  } else {
+    console.log('Usage:');
+    console.log('node generate-toml.js <templateName> <.dot name> <output name>');
+  }
+
 }
 
 main()
