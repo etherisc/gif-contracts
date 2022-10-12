0b63d0d20e4406e5b0e1eabf299590086535c611
Marc Doerflinger <marc@doerf.li>
2022-10-12 13:21:57 +0000

    add tests (#53)

--- tests/test_policy_adjust_premium.py
+++ tests/test_policy_adjust_premium.py
@@ -343,3 +343,115 @@ def test_adjust_premium_premium_too_big(
     with brownie.reverts("ERROR:POC-025:APPLICATION_PREMIUM_INVALID"):
         product.adjustPremiumSumInsured(processId, adjustedPremium, sumInsured, {'from': customer})
         
+
+def test_adjust_premium_premium_too_small(
+    instance: GifInstance, 
+    testCoin,
+    gifTestProduct: GifTestProduct, 
+    riskpoolKeeper: Account,
+    owner: Account,
+    customer: Account,
+    capitalOwner: Account,
+    insurer: Account
+):
+    # prepare funded riskpool
+    riskpool = gifTestProduct.getRiskpool().getContract()
+    initialFunding = 15000
+    fund_riskpool(instance, owner, capitalOwner, riskpool, riskpoolKeeper, testCoin, initialFunding)
+
+    # record number of policies before policy creation
+    product = gifTestProduct.getContract()
+    
+    # application spec
+    premium = 100
+    premiumPaid = 0
+    sumInsured = 5000
+    metaData = bytes(0)
+    applicationData = bytes(0)
+
+    
+    # create policies
+    process_tx = product.applyForPolicy(
+        premium,
+        sumInsured,
+        metaData,
+        applicationData,
+        {'from': customer})
+    processId = process_tx.return_value
+
+    # check policy state
+    policyController = instance.getPolicy()
+    application = policyController.getApplication(processId).dict()
+    policy = policyController.getPolicy(processId).dict()
+
+    assert application['state'] == 2
+    assert application['premiumAmount'] == premium
+    assert application['sumInsuredAmount'] == sumInsured
+    assert policy['premiumExpectedAmount'] == premium
+    assert policy['premiumPaidAmount'] == premiumPaid
+
+    # ensure adjustment cannot increase premium to amount > sumInsured
+    adjustedPremium = 6000
+    with brownie.reverts("ERROR:POC-025:APPLICATION_PREMIUM_INVALID"):
+        product.adjustPremiumSumInsured(processId, adjustedPremium, sumInsured, {'from': customer})
+
+    
+    # ensure adjustment cannot decrease premium to amount < already paid amount
+    adjustedPremium = 0
+    with brownie.reverts("ERROR:POC-025:APPLICATION_PREMIUM_INVALID"):
+        product.adjustPremiumSumInsured(processId, adjustedPremium, sumInsured, {'from': customer})
+        
+
+def test_adjust_premium_sumInsured_too_small(
+    instance: GifInstance, 
+    testCoin,
+    gifTestProduct: GifTestProduct, 
+    riskpoolKeeper: Account,
+    owner: Account,
+    customer: Account,
+    capitalOwner: Account,
+    insurer: Account
+):
+    # prepare funded riskpool
+    riskpool = gifTestProduct.getRiskpool().getContract()
+    initialFunding = 15000
+    fund_riskpool(instance, owner, capitalOwner, riskpool, riskpoolKeeper, testCoin, initialFunding)
+
+    # record number of policies before policy creation
+    product = gifTestProduct.getContract()
+    
+    # application spec
+    premium = 100
+    sumInsured = 5000
+    metaData = bytes(0)
+    applicationData = bytes(0)
+
+    # transfer funds to customer and create allowance
+    testCoin.transfer(customer, 2 * premium, {'from': owner})
+    testCoin.approve(instance.getTreasury(), 2 * premium, {'from': customer})
+
+    # create policies
+    process_tx = product.applyForPolicy(
+        premium,
+        sumInsured,
+        metaData,
+        applicationData,
+        {'from': customer})
+    processId = process_tx.return_value
+
+
+    # check policy state
+    policyController = instance.getPolicy()
+    application = policyController.getApplication(processId).dict()
+    policy = policyController.getPolicy(processId).dict()
+
+    assert application['state'] == 2
+    assert application['premiumAmount'] == premium
+    assert application['sumInsuredAmount'] == sumInsured
+    assert policy['premiumExpectedAmount'] == premium
+
+    # adjust premium of policy    
+    adjustedSumInsured = 50
+    with brownie.reverts("ERROR:POC-025:APPLICATION_PREMIUM_INVALID"):
+        tx = product.adjustPremiumSumInsured(processId, premium, adjustedSumInsured, {'from': customer})
+
