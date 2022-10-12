b9b63abf202504046cc90d952d41df3781979150
Marc Doerflinger <marc@doerf.li>
2022-10-12 13:09:21 +0000

    Add tests for adjusting premium (#53)

--- tests/test_policy_adjust_premium.py
+++ tests/test_policy_adjust_premium.py
@@ -1,4 +1,5 @@
 import pytest
+import brownie
 
 from brownie.network.account import Account
 from brownie import (
@@ -15,6 +16,10 @@ from scripts.instance import (
 
 from scripts.product import (
     GifTestProduct,
+)
+
+from scripts.util import (
+    s2b32
 )
 
 # enforce function isolation for tests below
@@ -48,8 +53,6 @@ def test_adjust_premium(
     # transfer funds to customer and create allowance
     testCoin.transfer(customer, 2 * premium, {'from': owner})
     testCoin.approve(instance.getTreasury(), 2 * premium, {'from': customer})
-
-    instanceService = instance.getInstanceService()
 
     # create policies
     process_tx = product.applyForPolicy(
@@ -118,8 +121,6 @@ def test_adjust_sumInsured(
     testCoin.transfer(customer, 2 * premium, {'from': owner})
     testCoin.approve(instance.getTreasury(), 2 * premium, {'from': customer})
 
-    instanceService = instance.getInstanceService()
-
     # create policies
     process_tx = product.applyForPolicy(
         premium,
@@ -187,8 +188,6 @@ def test_adjust_premium_and_sumInsured(
     testCoin.transfer(customer, 2 * premium, {'from': owner})
     testCoin.approve(instance.getTreasury(), 2 * premium, {'from': customer})
 
-    instanceService = instance.getInstanceService()
-
     # create policies
     process_tx = product.applyForPolicy(
         premium,
@@ -229,3 +228,118 @@ def test_adjust_premium_and_sumInsured(
     assert policy['premiumExpectedAmount'] == adjustedPremium
     assert policy['payoutMaxAmount'] == adjustedSumInsured
 
+
+def test_adjust_premium_sumInsured_too_big(
+    instance: GifInstance, 
+    testCoin,
+    gifTestProduct: GifTestProduct, 
+    riskpoolKeeper: Account,
+    owner: Account,
+    customer: Account,
+    capitalOwner: Account
+):
+    # prepare funded riskpool
+    riskpool = gifTestProduct.getRiskpool().getContract()
+    initialFunding = 15000
+    fund_riskpool(instance, owner, capitalOwner, riskpool, riskpoolKeeper, testCoin, initialFunding)
+
+    # record number of policies before policy creation
+    product = gifTestProduct.getContract()
+    
+    # application spec
+    premium = 100
+    sumInsured = 5000
+    metaData = bytes(0)
+    applicationData = bytes(0)
+
+    # transfer funds to customer and create allowance
+    testCoin.transfer(customer, 2 * premium, {'from': owner})
+    testCoin.approve(instance.getTreasury(), 2 * premium, {'from': customer})
+
+    # create policies
+    process_tx = product.applyForPolicy(
+        premium,
+        sumInsured,
+        metaData,
+        applicationData,
+        {'from': customer})
+    processId = process_tx.return_value
+
+    # check policy state
+    policyController = instance.getPolicy()
+    application = policyController.getApplication(processId).dict()
+    policy = policyController.getPolicy(processId).dict()
+
+    assert application['state'] == 2
+    assert application['premiumAmount'] == premium
+    assert application['sumInsuredAmount'] == sumInsured
+    assert policy['premiumExpectedAmount'] == premium
+
+    # ensure adjustment cannot increase sum insured
+    adjustSumInsured = 6000
+    with brownie.reverts("ERROR:POC-026:APPLICATION_SUM_INSURED_INCREASE_INVALID"):
+        product.adjustPremiumSumInsured(processId, premium, adjustSumInsured, {'from': customer})
+
+
+def test_adjust_premium_premium_too_big(
+    instance: GifInstance, 
+    testCoin,
+    gifTestProduct: GifTestProduct, 
+    riskpoolKeeper: Account,
+    owner: Account,
+    customer: Account,
+    capitalOwner: Account,
+    insurer: Account
+):
+    # prepare funded riskpool
+    riskpool = gifTestProduct.getRiskpool().getContract()
+    initialFunding = 15000
+    fund_riskpool(instance, owner, capitalOwner, riskpool, riskpoolKeeper, testCoin, initialFunding)
+
+    # record number of policies before policy creation
+    product = gifTestProduct.getContract()
+    
+    # application spec
+    premium = 100
+    premiumPaid = 50
+    sumInsured = 5000
+    metaData = bytes(0)
+    applicationData = bytes(0)
+
+    # transfer funds to customer and create allowance
+    testCoin.transfer(customer, 2 * premium, {'from': owner})
+    testCoin.approve(instance.getTreasury(), 50, {'from': customer})
+
+    # create policies
+    process_tx = product.applyForPolicy(
+        premium,
+        sumInsured,
+        metaData,
+        applicationData,
+        {'from': customer})
+    processId = process_tx.return_value
+
+    product.collectPremium(processId, premiumPaid)
+
+    # check policy state
+    policyController = instance.getPolicy()
+    application = policyController.getApplication(processId).dict()
+    policy = policyController.getPolicy(processId).dict()
+
+    assert application['state'] == 2
+    assert application['premiumAmount'] == premium
+    assert application['sumInsuredAmount'] == sumInsured
+    assert policy['premiumExpectedAmount'] == premium
+    assert policy['premiumPaidAmount'] == premiumPaid
+
+    # ensure adjustment cannot increase premium to amount > sumInsured
+    adjustedPremium = 6000
+    with brownie.reverts("ERROR:POC-025:APPLICATION_PREMIUM_INVALID"):
+        product.adjustPremiumSumInsured(processId, adjustedPremium, sumInsured, {'from': customer})
+
+    
+    # ensure adjustment cannot decrease premium to amount < already paid amount
+    adjustedPremium = 40
+    with brownie.reverts("ERROR:POC-025:APPLICATION_PREMIUM_INVALID"):
+        product.adjustPremiumSumInsured(processId, adjustedPremium, sumInsured, {'from': customer})
+        
