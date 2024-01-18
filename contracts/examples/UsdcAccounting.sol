c1375c5acadb6ba28ef646ebb701032f0b9b13b7
Matthias Zimmermann <zimmermann@xbrain.ch>
2024-01-18 09:51:00 +0000

    adapt devcontainer setup for mac/rosetta

--- contracts/examples/UsdcAccounting.sol
+++ contracts/examples/UsdcAccounting.sol
@@ -7,8 +7,7 @@ contract UsdcAccounting is ERC20 {
 
     string public constant NAME = "USD Coin - Accounting Token";
     string public constant SYMBOL = "USDC";
-    uint256 public constant DECIMALS = 6;
-
+    uint8 public constant DECIMALS = 6;
     uint256 public constant INITIAL_SUPPLY = 10**24;
 
     constructor()
@@ -20,7 +19,7 @@ contract UsdcAccounting is ERC20 {
         );
     }
 
-    function decimals() external view returns (uint256) {
+    function decimals() public view virtual override returns (uint8) {
         return DECIMALS;
     }
 }
