0699d8217ca4c34fab271b0157b2bb38bec453bc
Matthias Zimmermann <zimmermann@xbrain.ch>
2024-01-18 10:11:34 +0100

    add UsdcAccounting token

---
+++ contracts/examples/UsdcAccounting.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract UsdcAccounting is ERC20 {
+
+    string public constant NAME = "USD Coin - Accounting Token";
+    string public constant SYMBOL = "USDC";
+    uint256 public constant DECIMALS = 6;
+
+    uint256 public constant INITIAL_SUPPLY = 10**24;
+
+    constructor()
+        ERC20(NAME, SYMBOL)
+    {
+        _mint(
+            _msgSender(),
+            INITIAL_SUPPLY
+        );
+    }
+
+    function decimals() external view returns (uint256) {
+        return DECIMALS;
+    }
+}
